5402e63187080d6838b665eb4f4c925a25602836
Fernandes Samuel <samuel.fernandes@etu.unistra.fr>
2025-08-09 21:52:16 +0200

    add antlr to parse uml

--- PumlPlaner/UnitTest1.cs
+++ PumlPlaner/UnitTest1.cs
@@ -1,4 +1,6 @@
-﻿namespace PumlPlaner;
+﻿using Antlr4.Runtime;
+
+namespace PumlPlaner;
 
 public class Tests
 {
@@ -10,6 +12,14 @@ public class Tests
     [Test]
     public void Test1()
     {
+        var input = CharStreams.fromString("@startuml\n\nclass Fruit {\n  - vitamins int\n  + eat()\n}\n\n@enduml\n");
+        var lexer = new PumlgLexer(input);
+        var tokens = new CommonTokenStream(lexer);
+        var parser = new PumlgParser(tokens);
+        var tree = parser.uml(); // 'diagram' = point d'entrée dans la grammaire
+
+        Console.WriteLine(tree.ToStringTree());
+
         Assert.Pass();
     }
 }
